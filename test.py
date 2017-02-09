b9b89e18e905267370e30e1f57c51ba07fbd8c61
James Greve <jgreve@livescribe.com>
2017-02-09 11:47:52 -0800

    Pycharm

---
+++ test.py
@@ -0,0 +1,13 @@
+def letters(*args):
+    for arg in args:
+        morphed = arg.split()
+        upper = arg.upper()
+        print(morphed[0] + upper)
+
+
+def nums(*args):
+    args = list(args)
+
+    letters(*args)
+
+nums('air', 'water', 'earth')
